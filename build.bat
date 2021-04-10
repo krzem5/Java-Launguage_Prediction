4d65c47254e7c483be4c22e73a68042a46984130
Krzem <krzem5.dev@gmail.com>
2021-04-10 21:09:06 +0200

    Push Update 04/10/2021, 21:08:58

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/launguage_prediction/Main.java&&jar cvmf ../manifest.mf ../build/launguage_prediction.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="launguage_prediction.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/launguage_prediction.jar
-:end
+python build.py %*
